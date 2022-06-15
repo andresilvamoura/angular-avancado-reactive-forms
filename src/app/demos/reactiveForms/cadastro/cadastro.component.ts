08f26bfa569ad3a38b0e92252d06cc2be08251e7
Andre Moura <andremoura500@gmail.com>
2022-06-15 08:00:52 -0300

    feat: Adicionado Validação de formulário Customizadas com libs

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgBrazilValidators } from 'ng-brazil';
 import { Usuario } from './models/usuario';
+import { utilsBr } from 'js-brasil'
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-cadastro',
@@ -12,21 +15,25 @@ export class CadastroComponent implements OnInit {
   cadastroForm: FormGroup;
   usuario: Usuario;
   formResult: string = '';
+  MASKS = utilsBr.MASKS;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
+    let senhaConfirmacao = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15]), CustomValidators.equalTo(senha)]);
+
     this.cadastroForm = this.formBuilder.group({
       nome: ['', Validators.required],
-      cpf: [''],
-      email: ['',[Validators.required, Validators.email]],
-      senha: [''],
-      senhaConfirmacao: ['']
+      cpf: ['', [Validators.required, NgBrazilValidators.cpf]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: senha,
+      senhaConfirmacao: senhaConfirmacao
     });
   }
 
-  adicionarUsuario(){
-    if(this.cadastroForm.dirty && this.cadastroForm.valid){
+  adicionarUsuario() {
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
       this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
       this.formResult = JSON.stringify(this.cadastroForm.value);
     } else {
