74cb801577e45d3a5d1bfca3030f74d355e1a9cb
Andre Moura <andremoura500@gmail.com>
2022-06-15 06:40:58 -0300

    feat: Adicionado condição para criar o objeto usuario, e botão de submit desabilitado

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -11,6 +11,7 @@ export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
   usuario: Usuario;
+  formResult: string = '';
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -25,7 +26,12 @@ export class CadastroComponent implements OnInit {
   }
 
   adicionarUsuario(){
-    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+    if(this.cadastroForm.dirty && this.cadastroForm.valid){
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+    } else {
+      this.formResult = "Não submeteu!!!"
+    }
   }
 
 }
